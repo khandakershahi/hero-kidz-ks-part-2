ee6ea779cfd9e1d6fdf03c50bf4d5a85e8b07159
khandakershahi <khandakershahi@gmail.com>
2025-12-29 01:54:00 +0600

    all running ok now.

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -27,7 +27,6 @@ const geistMono = Geist_Mono({
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-
       <body
         className={`${poppins.className} ${geistSans.variable} ${geistMono.variable} antialiased`}
       >
